24fa35195b0918a85eb0b83724ed598ddfeab18b
炳源 <bingyuango@gmail.com>
2025-08-17 02:45:47 +0800

    feat: 存储文件夹结构

--- LibLink/internal/models/archive/archive.go
+++ LibLink/internal/models/archive/archive.go
@@ -1,6 +1,8 @@
 package archive
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type Archive struct {
 	gorm.Model
@@ -11,4 +13,35 @@ type Archive struct {
 	ArcType     string `gorm:"column:arc_type;comment:'文献类型'" json:"arc_type"`
 	BorrowState string `gorm:"column:borrow_state;comment:'借阅状态'" json:"borrow_state"`
 	FolderID    uint   `gorm:"column:folder_id;comment:'文件夹ID'" json:"folder_id"`
+}
+
+type Folder struct {
+	gorm.Model
+	Name            string `gorm:"column:name;comment:'文件夹名称'" json:"name"`
+	Path            string `gorm:"column:path;comment:'文件夹路径'" json:"path"`
+	ParentID        uint   `gorm:"column:parent_id;index:idx_parent_id;comment:'父文件夹ID'" json:"parent_id"`
+	GroupPermission string `gorm:"column:group_permission;comment:'用户组权限,自动继承父文件夹权限,需要有其中所有权限才能够访问该文件夹'" json:"group_permission"`
+}
+
+// FileFolders 用于查询文件夹和档案的结构体
+type FileFolders struct {
+	FileFolders []FileFolders `json:"file_folders"` // 文件夹列表
+	Archives    []Archive     `json:"archives"`     // 当前文件夹下的档案列表
+}
+
+func GetFoldersAndFilesByParentID(DB *gorm.DB, parentID uint, permission string) (*FileFolders, error) {
+	var fileFolders FileFolders
+	var archives []Archive
+	// 查询当前文件夹底下的所有文件
+	err := DB.Model(&Archive{}).Where("parent_id = ? AND FIND_IN_SET(?, group_permission)", parentID, permission).Find(&archives).Error
+	if err != nil {
+		return nil, err
+	}
+	// 查询当前文件夹底下的所有文件夹
+	err = DB.Model(&Folder{}).Where("parent_id = ? AND FIND_IN_SET(?, group_permission)", parentID, permission).Find(&fileFolders).Error
+	if err != nil {
+		return nil, err
+	}
+	fileFolders.Archives = archives
+	return &fileFolders, nil
 }
--- LibLink/internal/models/user/user.go
+++ LibLink/internal/models/user/user.go
@@ -9,5 +9,11 @@ type User struct {
 	Username string `gorm:"column:username;comment:'用户名'"`
 	Password string `gorm:"column:password;comment:'密码'"`
 	Email    string `gorm:"column:email;comment:'email/唯一标识符'"`
-	Role     string `gorm:"column:role;comment:'角色，admin与user';default:user"`
+	Role     string `gorm:"column:role;comment:'角色,admin与user';default:user"`
+}
+
+type UserGroup struct {
+	gorm.Model
+	Name        string `gorm:"column:name;comment:'用户组名称'"`
+	Description string `gorm:"column:description;comment:'用户组描述'"`
 }
